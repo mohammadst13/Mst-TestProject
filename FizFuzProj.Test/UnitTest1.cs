bf075dc7ca3e470df1ceef44d828930531ca25da
mohammad st <mohammad.sayartehrani@yahoo.com>
2021-04-08 23:26:35 +0430

    Test Complete

--- FizFuzProj.Test/UnitTest1.cs
+++ FizFuzProj.Test/UnitTest1.cs
@@ -6,9 +6,67 @@ namespace FizFuzProj.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Execute_Expect1_when1()
         {
-
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(1);
+            Assert.Equal("1", result);
+        }
+        [Fact]
+        public void Execute_Expect12_when2()
+        {
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(2);
+            Assert.Equal("1,2", result);
+        }
+        [Fact]
+        public void Execute_Expect12Fiz_when3()
+        {
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(3);
+            Assert.Equal("1,2,Fiz", result);
+        }
+        [Fact]
+        public void Execute_Expect12Fiz4_when4()
+        {
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(4);
+            Assert.Equal("1,2,Fiz,4", result);
+        }
+        [Fact]
+        public void Execute_Expect12Fiz4Fuz_when5()
+        {
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(5);
+            Assert.Equal("1,2,Fiz,4,Fuz", result);
+        }
+        [Fact]
+        public void Execute_Expect12Fiz4FuzFiz_when6()
+        {
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(6);
+            Assert.Equal("1,2,Fiz,4,Fuz,Fiz", result);
+        }
+        [Fact]
+        public void Execute_Expect12Fiz4FuzFiz_when10()
+        {
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(10);
+            Assert.Equal("1,2,Fiz,4,Fuz,Fiz,7,8,Fiz,Fuz", result);
+        }
+        [Fact]
+        public void Execute_Expect12Fiz4FuzFiz_when15()
+        {
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(15);
+            Assert.Equal("1,2,Fiz,4,Fuz,Fiz,7,8,Fiz,Fuz,11,Fiz,13,14,FizFuz", result);
+        }
+        [Fact]
+        public void Execute_Expect12Fiz4FuzFiz_when30()
+        {
+            var fizfuzz = new FizFuz();
+            string result = fizfuzz.Execute(30);
+            Assert.Equal("1,2,Fiz,4,Fuz,Fiz,7,8,Fiz,Fuz,11,Fiz,13,14,FizFuz,16,17,Fiz,19,Fuz,Fiz,22,23,Fiz,Fuz,26,Fiz,28,29,FizFuz", result);
         }
     }
 }
--- FizFuzProj/Class1.cs
+++ FizFuzProj/Class1.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizFuzProj
 {
-    public class Class1
+    public class FizFuz
     {
+
+        public string Execute(int input)
+        {
+            List<object> result = new List<object>();
+            for (int i = 1; i <= input; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    result.Add("FizFuz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Fuz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fiz");
+                }
+                else
+                {
+                    result.Add(i);
+                }
+            }
+            return string.Join(",",result.ToArray());
+        }
     }
 }
